32eecde87efca10d70a2c2ccb62014edbe3860fe
Dorette <dorette.schutte86@gmail.com>
2023-10-30 15:55:07 +0200

    adding code

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://doretteschutte86:DVigLDAGoqZeQPFb@cluster0.7tlofzz.mongodb.net/test?retryWrites=true&w=majority"
-}
+  url: "mongodb+srv://doretteschutte86:DVigLDAGoqZeQPFb@cluster0.7tlofzz.mongodb.net/test?retryWrites=true&w=majority",
+};
